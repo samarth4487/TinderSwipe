92b86bec7650273a8780229528a1de35bec470e6
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2017-05-15 22:17:21 +0530

    Showing thumbs up and down on dragging the card

--- TinderSwipe/ViewController.swift
+++ TinderSwipe/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var thumbImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +22,30 @@ class ViewController: UIViewController {
         mainView.layer.cornerRadius = 10
     }
     
+    // NOTE: By default the alpha of thumbs image view is set to zero
+    
     @IBAction func mainViewPan(_ sender: UIPanGestureRecognizer) {
         let card = sender.view!
         let point = sender.translation(in: view)
         
         card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
         
+        if point.x > 0 {
+            thumbImageView.alpha = 1
+            thumbImageView.image = #imageLiteral(resourceName: "up")
+            thumbImageView.tintColor = UIColor.green
+        } else if point.x < 0 {
+            thumbImageView.alpha = 1
+            thumbImageView.image = #imageLiteral(resourceName: "down")
+            thumbImageView.tintColor = UIColor.red
+        }
+        
+        thumbImageView.alpha = abs(point.x) / view.center.x
+        
         if sender.state == UIGestureRecognizerState.ended {
             UIView.animate(withDuration: 0.2, animations: { 
                 card.center = self.view.center
+                self.thumbImageView.alpha = 0
             })
         }
     }
