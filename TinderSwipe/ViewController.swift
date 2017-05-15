96df07a50264d0bea7db655f92e948de4a3728fa
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2017-05-15 21:37:13 +0530

    Added functionality to move cards freely

--- TinderSwipe/ViewController.swift
+++ TinderSwipe/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainView.layer.masksToBounds = true
+        mainView.layer.cornerRadius = 10
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func mainViewPan(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        
+        if sender.state == UIGestureRecognizerState.ended {
+            UIView.animate(withDuration: 0.2, animations: { 
+                card.center = self.view.center
+            })
+        }
     }
-
-
 }
 
