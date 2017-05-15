f2e1bc18223fc2bc887fefd6e81a090936d8b50a
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2017-05-15 20:46:44 +0530

    Initial commit

---
+++ TinderSwipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderSwipe
+//
+//  Created by Samarth Paboowal on 15/05/17.
+//  Copyright © 2017 Junkie Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
